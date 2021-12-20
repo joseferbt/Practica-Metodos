bee626004a8c93bc8f115eac74c77fb7fe5decb3
jennyferbm13 <51867672+jennyferbm13@users.noreply.github.com>
2021-12-20 11:48:15 -0500

    grafica

--- main.py
+++ main.py
@@ -1,4 +1,72 @@
 import numpy as np
+import pip
+pip.main(["install","matplotlib"])
+import matplotlib.pylab as p;
+
+Nx = 10
+Ny = 10
+vy = np.zeros((Nx + 1, Ny + 1), float)
+vx = np.zeros((Nx + 1, Ny + 1), float)
+iter = 0
+pMasI = 1
+PMenosI = 1
+h = 1.0
+w = 1.0
+vo = 1.0
+a = " "
+
+for i in range(Nx):
+    for j in range(Ny):
+        if j == 0:
+            vx[i][j] = 0
+        else:
+            vx[i][j] = j
+
+
+def prueba1r(w, h):
+    for i in range(1, Nx):
+        for j in range(1, Ny):
+            r1 = w * (((1 / 4) * vx[i + 1][j] +
+                       (1 / 4) * vx[i - 1][j]
+                       + (1 / 4) * vx[i][j + 1]
+                       + (1 / 4) * vx[i][j - 1]
+                       - ((1 / 4) * (h / 2) * vy[i][j] * vx[i + 1][j]) + (
+                                   (1 / 4) * (h / 2) * vy[i][j] * vx[i - 1][j])  # es cero
+                       - ((1 / 4) * (h / 2) * vx[i][j] * vx[i][j + 1]) + ((1 / 4) * (h / 2) * vx[i][j] * vx[i][j - 1])
+                       - ((1 / 4) * (h / 2) * pMasI) + ((1 / 4) * (h / 2) * PMenosI)) - vx[i][j])
+            vx[i][j] = vx[i][j] + r1
+
+
+while (iter <= 6):
+    iter += 1
+    if iter % 10 == 0: print(iter)
+    prueba1r(w, h)
+
+for i in range(0, Nx + 1):
+    for j in range(0, Ny + 1): vx[i][j] = vx[i][j] / vo * h
+
+# print(vx)
+
+x = range(0, Nx - 1);
+y = range(0, Ny - 1)
+X, Y = p.meshgrid(x, y)
+
+
+def functz(u):
+    z = u[X, Y]
+    return z
+
+
+Z = functz(vx)
+
+fig, ax = p.subplots()
+
+ax.pcolormesh(X, Y, Z, vmin=-0.5, vmax=1.0)
+
+p.show()
+"""
+1
+import numpy as np
 
 Nx = 10
 Ny = 10
@@ -55,8 +123,8 @@ def imprimir(vx, a):
 
 # Se imprime la velocidad x
 print(imprimir(vx, a))
-
-""""import numpy as np
+2
+import numpy as np
 Nx = 10
 Ny = 10
 velx = np.zeros((Nx,Ny))

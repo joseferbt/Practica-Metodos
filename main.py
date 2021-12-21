96772ebe1278fa3f3b83d9aabcb91493eae17058
Jose Fernando Botina <joseferbt@gmail.com>
2021-12-21 08:40:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,78 @@
 import numpy as np
+#import pip pip.main(["install","matplotlib"])
+import matplotlib.pylab as p
+
+Nx = 10
+Ny = 10
+vy = np.zeros((Nx + 1, Ny + 1), float)
+vx = np.zeros((Nx + 1, Ny + 1), float)
+grid = np.zeros(Nx+1,Ny+1)
+iter = 0
+pMasI = 1
+PMenosI = 1
+h = 1.0
+w = 1.0
+vo = 1.0
+a = " "
+for i in range(Nx-5,Nx+1):
+    for j in range(Ny -5, Ny+1):
+        grid[i][j] = 1
+
+for i in range(Nx):
+    for j in range(Ny):
+        if j == 0:
+            vx[i][j] = 0
+        else:
+            if not grid[i][j] :
+                vx[i][j] = j
+
+
+
+def prueba1r(w, h):
+    for i in range(1, Nx):
+        for j in range(1, Ny):
+            r1 = w * (((1 / 4) * vx[i + 1][j] +
+                       (1 / 4) * vx[i - 1][j]
+                       + (1 / 4) * vx[i][j + 1]
+                       + (1 / 4) * vx[i][j - 1]
+                       - ((1 / 4) * (h / 2) * vy[i][j] * vx[i + 1][j]) + (
+                                   (1 / 4) * (h / 2) * vy[i][j] * vx[i - 1][j])  # es cero
+                       - ((1 / 4) * (h / 2) * vx[i][j] * vx[i][j + 1]) + ((1 / 4) * (h / 2) * vx[i][j] * vx[i][j - 1])
+                       - ((1 / 4) * (h / 2) * pMasI) + ((1 / 4) * (h / 2) * PMenosI)) - vx[i][j])
+            if not grid[i][j] :
+                vx[i][j] = vx[i][j] + r1
+
+
+while (iter <= 6):
+    iter += 1
+    if iter % 10 == 0: print(iter)
+    prueba1r(w, h)
+
+for i in range(0, Nx + 1):
+    for j in range(0, Ny + 1): vx[i][j] = vx[i][j] / vo * h
+
+# print(vx)
+
+x = range(0, Nx - 1);
+y = range(0, Ny - 1)
+X, Y = p.meshgrid(x, y)
+
+
+def functz(u):
+    z = u[X, Y]
+    return z
+
+
+Z = functz(vx)
+
+fig, ax = p.subplots()
+
+ax.pcolormesh(X, Y, Z, vmin=-0.5, vmax=1.0)
+
+p.show()
+"""
+1
+import numpy as np
 
 Nx = 10
 Ny = 10
@@ -52,12 +126,40 @@ def imprimir(vx, a):
         a += "\n"
     return a
 
-#Flujo constante durante n iteraciones n = itera
+# Se imprime la velocidad x
+print(imprimir(vx, a))
+
+2
+import numpy as np
+Nx = 10
+Ny = 10
+velx = np.zeros((Nx,Ny))
+auxVx = []
+p = 1
+h=1
+w=1
+for i in range(Nx):
+    auxVx.append([])
+    for j in range(Ny):
+        if(j==0):
+            velx[i][j]=1
+        auxVx[i].append(((j+1)))
+
+
+print(velx)
+
+for i in range(1,Nx-1):
+    for j in range(1,Ny-1):
+        velx[i][j] = velx[i][j] + w * 1/4*(auxVx[i+1][j]+auxVx[i-1][j]+auxVx[i][j+1]+auxVx[i][j-1]
+                                       -(h/2)*velx[i][j]*(auxVx[i+1][j]-auxVx[i-1][j])
+                                       -(h/2)*velx[i][j]*(auxVx[i][j+1]-auxVx[i][j-1])
+                                       -(h/2)*p*p)-velx[i][j]
 
 for i in range(5):
     pruebavx(w)
     print("[",i,"]")
     print(imprimir(vx, a))
 
-# Se imprime la velocidad x
+
+# Se imprime la velocidad x """
 
